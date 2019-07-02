db3143b668af70b70968e7ca02ed0212dc6ddf91
NotSimple <engarrido@uc.cl>
2019-07-02 00:28:39 -0400

    add more PermissionSystem tests

--- Smart Contracts/Interactions between Smart Contracts/test/tests.js
+++ Smart Contracts/Interactions between Smart Contracts/test/tests.js
@@ -7,28 +7,71 @@ const expectThrow = require('./helper.js');
 contract('PermissionSystem', async (accounts) => {
   let instance;
   let owner = accounts[0]; // owner of this contract
-  let userA = accounts[1]; // not owner of this contract
+  let non_owner_1 = accounts[1]; // not owner of this contract
 
 	beforeEach(async () => {
 		instance = await PermissionSystem.new();
   });
+
   context('Adding records', async () => {
     it('should allow owner to add a new record', async () => {
-      const newRecordHash = web3.utils.sha3('record 1');
-      await instance.addRecord(newRecordHash, {from: owner});
+      let newRecordHash = web3.utils.sha3('record 1');
+      await instance.addRecord(newRecordHash, {from: owner}); // add new record
     });
-  
+
     it('should not allow owner to add the same record more than once', async () => {
-      const recordHash = web3.utils.sha3('record 2');
-      await instance.addRecord(recordHash, {from: owner});
-      let tx = instance.addRecord(recordHash, {from: owner});
+      let recordHash = web3.utils.sha3('record 1');
+      await instance.addRecord(recordHash, {from: owner}); // add new record
+      let tx = instance.addRecord(recordHash, {from: owner}); // attempt to add it again
       await expectThrow(tx);
     });
-  
-    it('should not allow non-owner to add a new record', async () => {
-      recordHash = web3.utils.sha3('record 3');
-      tx = instance.addRecord(recordHash, {from: userA});
+
+    it('should not allow a non-owner to add a new record', async () => {
+      let recordHash = web3.utils.sha3('record 3');
+      tx = instance.addRecord(recordHash, {from: non_owner_1}); // attempt to add new record
       await expectThrow(tx);
     });
-  })
+  });
+
+  context('Creating PermissionManagers', async () => {
+    it('should allow owner to create a new permission manager contract', async () => {
+      let user = 'Alice'
+      let encodedUser = web3.utils.fromAscii(user)
+      await instance.createPermissionManager(encodedUser, {from: owner}); // create permission manager contract
+    });
+
+    it('should not allow owner to create a permission manager contract for a user who already has one', async () => {
+      let user = 'Boris'
+      let encodedUser = web3.utils.fromAscii(user)
+      await instance.createPermissionManager(encodedUser, {from: owner}); // create first permission manager contract
+      let tx = instance.createPermissionManager(encodedUser, {from: owner}); // attempt to create second permission manager contract for same user
+      await expectThrow(tx);
+    });
+
+    it('should not allow a non-owner to create a permission manager contract', async () => {
+      let user = 'Charlie'
+      let encodedUser = web3.utils.fromAscii(user)
+      let tx = instance.createPermissionManager(encodedUser, {from: non_owner_1});
+      await expectThrow(tx);
+    });
+  });
+
+  context('Granting permissions', async () => {
+    let exampleRecord = web3.utils.sha3('example record');
+    let encodedUser = web3.utils.fromAscii('Alice')
+
+    it('should allow owner to grant a new permission to a user with an existing permission manager contract', async () => {
+      await instance.addRecord(exampleRecord, {from: owner}) // add an example record
+      await instance.createPermissionManager(encodedUser, {from: owner}); // create permission manager contract
+      await instance.grantPermission(encodedUser, exampleRecord, {from: owner}) // grant permission
+    });
+
+    it('should not allow owner to grant a new permission to a user without an existing permission manager contract', async () => {
+      await instance.addRecord(exampleRecord, {from: owner}) // add an example record
+      let tx = instance.grantPermission(encodedUser, exampleRecord, {from: owner}) // grant permission
+      await expectThrow(tx);
+    });
+    
+  });
+
 });
